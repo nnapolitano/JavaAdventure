a43b191daa409a3d215c63faad04f831cba92c8c
nnapolitano <51179552+nnapolitano@users.noreply.github.com>
2019-08-28 20:02:34 -0400

    First commit.

---
+++ src/com/cscc/javaadventure/JavaAdventure.java
@@ -0,0 +1,9 @@
+package com.cscc.javaadventure;
+
+public class JavaAdventure {
+    public static void main(String[] args){
+        System.out.println ("Welcome to Java Adventure! ");
+
+    }
+
+}
